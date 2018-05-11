c1c304e268dfcf49d2147f1e1415237937e66b35
Jan Burle <jan@c53.be>
2018-05-11 22:18:53 +0200

    statcounter

--- stats.php
+++ stats.php
@@ -5,9 +5,9 @@ if ($_SERVER['SERVER_PORT'] < 8000 /* not debug */) {
 
   $book['page_end'] = <<<HEREDOC
 <script type="text/javascript">
-var sc_project=11685427;
+var sc_project=11709078;
 var sc_invisible=1;
-var sc_security="bc2e88ea";
+var sc_security="86bf9c2a";
 var scJsHost = (("https:" == document.location.protocol) ? "https://secure." : "http://www.");
 document.write("<sc"+"ript type='text/javascript' src='" + scJsHost + "statcounter.com/counter/counter.js'></"+"script>");
 </script>
